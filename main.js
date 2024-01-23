9d7e4f6a5032d37a06dfddb3b2f6201ab91fd553
MMonzon00 <martinmonzon00@gmail.com>
2024-01-23 02:30:14 -0300

    one of the finals

--- main.js
+++ main.js
@@ -4,224 +4,196 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const loader = new GLTFLoader();
-var startingCoordinates = { x: 0, y: 5, z: 0 };
-// let both;
-// loader.load(
-//     './models/scene.gltf',function(gltf){
-//         both=gltf.scene;
-//         scene.add(both);
-//     },
-// );
+let myModel;
+let controls;
 
-let CyberTruck;
 loader.load(
-    './models/cybertruckFinal.glb',
-    function (gltf) {
-      CyberTruck = gltf.scene;
-      CyberTruck.scale.set(7, 7, 7);
-      CyberTruck.position.set(0, 0, 0);
-      scene.add(CyberTruck);
-      setupGuiControls();
-    },
-    // ... (progress and error callbacks)
-  );
-  
-  let Garage;
+  './models/cybertruckFinal.glb',
+  function (gltf) {
+    myModel = gltf.scene;
+    myModel.scale.set(7, 7, 7);
+    scene.add(myModel);
+  }
+);
 
-  loader.load(
-      './models/Garage1.glb',
-      function (gltf) {
-          Garage = gltf.scene;
-          Garage.position.set(0, 0, 0);
-          Garage.scale.set(10, 10, 10);
-          scene.add(Garage);
-      },
-      // ... (progress and error callbacks)
-  );
+let road;
+loader.load(
+  './models/Garage.glb',
+  function (gltf) {
+    road = gltf.scene;
+    road.position.y = -0.9;
+    road.scale.set(10, 10, 10);
+    scene.add(road);
+  }
+);
 
-//Add ambient light
-const ambientLight = new THREE.AmbientLight(0xFFFFFF,1);
-ambientLight.position.set(5,5,5);
-scene.add(ambientLight);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(0, 1, 0);
+scene.add(directionalLight);
 
-// Create a camera
-const startingCoordinatesCamera = {x: -119.56577588279379, y: 27.17728918932099, z: 32.14970851068203};
-const povCoords = {x: -44.21602280180338, y: 12.710755362794542, z: 31.43912203114417}
-const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(startingCoordinatesCamera.x, startingCoordinatesCamera.y, startingCoordinatesCamera.z);
-camera.lookAt(scene.position);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
 
-// Flag to toggle camera position
-let isInVehicleView = false;
-
-document.addEventListener('keydown', (event) => {
-    if (event.key === 'c' || event.key === 'C') {
-        isInVehicleView = !isInVehicleView;
-
-        if (isInVehicleView) {
-            // Set camera inside the vehicle (adjust position based on your model)
-            camera.position.set(povCoords.x, povCoords.y, povCoords.z);
-            camera.rotateY(180)
-            // Optional: Point the camera towards the front of the vehicle
-            const frontOfVehicle = new THREE.Vector3(startingCoordinatesCamera.x, startingCoordinatesCamera.y, startingCoordinatesCamera.z);
-            const rotationMatrix = new THREE.Matrix4();
-            rotationMatrix.lookAt(camera.position, frontOfVehicle, new THREE.Vector3(0, 1, 0));
-            const rotation = new THREE.Quaternion();
-            rotation.setFromRotationMatrix(rotationMatrix);
-            camera.quaternion.copy(rotation);
-        } else {
-            // Set camera to external view
-            camera.position.set(startingCoordinatesCamera.x, startingCoordinatesCamera.y, startingCoordinatesCamera.z);
-            camera.lookAt(scene.position);
-
-        }
-    }
-});
-
-// Create a renderer
 const renderer = new THREE.WebGLRenderer();
-document.body.appendChild(renderer.domElement);
 renderer.setSize(window.innerWidth, window.innerHeight);
-
-
-// add a pointLight
-const CoordinatesLight = {x: -46.297855604328355, y: 13.870796319255302, z: 31.29556735021375}
-const pointLight = new THREE.PointLight(0xffffff, 100000, 10); // color, intensity, distance
-pointLight.position.set(startingCoordinatesCamera.x, startingCoordinatesCamera.y, startingCoordinatesCamera.z); // Position of the light
-scene.add(pointLight);
-
-// Rotation variables
-const rotationSpeed = 0.02;
-
-
-// Set up keyboard input
-const keys = {};
-document.addEventListener('keydown', (event) => {
-    keys[event.key] = true;
-});
-
-document.addEventListener('keyup', (event) => {
-    keys[event.key] = false;
-});
-
-
-function setupGuiControls() {
-    const gui = new dat.GUI();
-
-    // Add dat.GUI controls for CyberTruck
-    const cyberTruckFolder = gui.addFolder('CyberTruck Controls');
-    const cyberTruckPosition = cyberTruckFolder.addFolder('Position');
-    const cyberTruckRotation = cyberTruckFolder.addFolder('Rotation');
-
-    // Initial values
-    let initialPosition = { x: 0, y: 0, z: 0 };
-
-    // Add position controls
-    cyberTruckPosition.add(CyberTruck.position, 'x', -100, 100).step(0.1).name('X').setValue(initialPosition.x);
-    cyberTruckPosition.add(CyberTruck.position, 'y', -100, 100).step(0.1).name('Y').setValue(initialPosition.y);
-    cyberTruckPosition.add(CyberTruck.position, 'z', -100, 100).step(0.1).name('Z').setValue(initialPosition.z);
-
-    // Add rotation controls
-    // (Add your rotation controls here if needed)
-
-    // Open the folders
-    cyberTruckFolder.open();
-    cyberTruckPosition.open();
-    cyberTruckRotation.open();
-}
-
-
-const controls = new OrbitControls(camera, renderer.domElement);
-// controls.target.copy(CyberTruck.position);
-// You can adjust these settings to control the sensitivity of the controls
-controls.rotateSpeed = 1.0;
-controls.zoomSpeed = 1.2;
-controls.panSpeed = 1;
-
-// Optional: How far or how close can the camera zoom
-controls.minDistance = 1;
-controls.maxDistance = 500;
-
-// Optional: To limit the vertical orbit angle (radians)
-controls.minPolarAngle = 0;
-controls.maxPolarAngle = Math.PI/2.01;
-
-// Optional: To enable damping (inertia), which can give a smoother control feel
-controls.enableDamping = true;
-controls.dampingFactor = 0.05;
-
-//////////////////////////////////////////////////////////////////////
-// Create coordinate axes
-const axesHelper = new THREE.AxesHelper(5);
-scene.add(axesHelper);
-
-// Create lines for specific coordinates
-const lineMaterial = new THREE.LineBasicMaterial({ color: 0x0000ff });
-const lineGeometryX = new THREE.BufferGeometry().setFromPoints([new THREE.Vector3(0, 0, 0), new THREE.Vector3(1, 0, 0)]);
-const lineGeometryY = new THREE.BufferGeometry().setFromPoints([new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 1, 0)]);
-const lineGeometryZ = new THREE.BufferGeometry().setFromPoints([new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, 1)]);
-
-const lineX = new THREE.Line(lineGeometryX, lineMaterial);
-const lineY = new THREE.Line(lineGeometryY, lineMaterial);
-const lineZ = new THREE.Line(lineGeometryZ, lineMaterial);
-
-scene.add(lineX);
-scene.add(lineY);
-scene.add(lineZ);
-//////////////////////////////////////////////////////////////////////
+document.body.appendChild(renderer.domElement);
 
 const loaderBack = new THREE.TextureLoader();
 
 loaderBack.load(
-    './backgrounds/skyboxStar.jpg', // Replace with your image path
-    function (texture) {
-        scene.background = texture; // Set the scene background to the loaded texture
-    },
-    undefined, // Optional: onProgress callback
-    function (error) {
-        console.error('An error happened during loading texture:', error);
-    }
+  './backgrounds/skyboxStars.jpg',
+  function (texture) {
+    scene.background = texture;
+  },
+  undefined,
+  function (error) {
+    console.error('An error happened during loading texture:', error);
+  }
 );
 
-let cameraOffset = new THREE.Vector3(0,15, 25); // Notice the z-value is positive now
+const keys = {
+  ArrowUp: false,
+  ArrowDown: false,
+  ArrowLeft: false,
+  ArrowRight: false,
+  Space: false,
+  c: false
+};
 
-const moveSpeed = 0.3;
-const rotateSpeed = 0.012;
+let cameraOffset = new THREE.Vector3(0, 20, -100);
 
-// Set up key input
 document.addEventListener('keydown', (event) => {
-    switch (event.key) {
-        case 'z':
-            rotateCameraAroundPoint();
-            break;
+    if (keys[event.key] !== undefined) {
+      keys[event.key] = true;
     }
+  
+    if (event.key === ' ' || event.key === 'Spacebar') {
+      keys.Space = true;
+      cameraOffset = new THREE.Vector3(0, 20, -100);
+    }
+  
+    if (event.key === 'c') {
+      keys.cPressed = !keys.cPressed; // Toggle the 'c' key status
+      console.log("'c' key status:", keys.cPressed);
+  
+      if (keys.cPressed) {
+        controls = new OrbitControls(camera, renderer.domElement);
+        controls.enableDamping = true;
+        controls.dampingFactor = 0.25;
+        controls.screenSpacePanning = false;
+        controls.maxPolarAngle = Math.PI / 2;
+        controls.target.copy(myModel.position);
+      } else {
+        controls.dispose();
+        controls = null;
+      }
+    }
+  });
+  
+  document.addEventListener('keyup', (event) => {
+    if (keys[event.key] !== undefined) {
+      keys[event.key] = false;
+    }
+  
+    if (event.key === ' ' || event.key === 'Spacebar') {
+      keys.Space = false;
+      cameraOffset = new THREE.Vector3(0, 20, 100);
+    }
+  });
+  
+  document.addEventListener('keyup', (event) => {
+    if (keys[event.key] !== undefined) {
+      keys[event.key] = false;
+    }
+  
+    if (event.key === ' ' || event.key === 'Spacebar') {
+      keys.Space = false;
+      cameraOffset = new THREE.Vector3(0, 20, 100);
+    }
+  });
+  
+  
+  document.addEventListener('keyup', (event) => {
+    if (keys[event.key] !== undefined) {
+      keys[event.key] = false;
+    }
+  
+    if (event.key === ' ' || event.key === 'Spacebar') {
+      keys.Space = false;
+      cameraOffset = new THREE.Vector3(0, 20, 100);
+    }
+  
+    if (event.key === 'c') {
+      keys.cPressed = false;
+      console.log("'c' key status:", keys.cPressed);
+    }
+  });
+
+document.addEventListener('keyup', (event) => {
+  if (keys[event.key] !== undefined) {
+    keys[event.key] = false;
+  }
+
+  if (event.key === ' ' || event.key === 'Spacebar') {
+    keys.Space = false;
+    cameraOffset = new THREE.Vector3(0, 20, 100);
+  }
+
+  if (event.key === 'c') {
+    keys.c = false;
+    console.log("'c' key status:", keys.c);
+  }
 });
 
-// Function to rotate the camera around a specific point (e.g., origin)
-function rotateCameraAroundPoint() {
-    const rotateCoords = {x: -42.12114255160086, y: 1.958620733852893, z: 28.067148101582536}
-    const pivot = new THREE.Vector3(rotateCoords.x, rotateCoords.y, rotateCoords.z); // The point to rotate around
+const moveSpeed = 1;
+const rotateSpeed = 0.012;
 
-    // Calculate the current distance and angle
-    const distance = camera.position.distanceTo(pivot);
-    const angle = Math.atan2(camera.position.x - pivot.x, camera.position.z - pivot.z);
-
-    // Update the camera's position and rotation
-    camera.position.x = pivot.x + distance * Math.cos(angle + 0.1); // 0.1 is the rotation angle
-    camera.position.z = pivot.z + distance * Math.sin(angle + 0.1);
-    camera.lookAt(pivot);
+function updatemyModel() {
+  if (keys.ArrowUp) {
+    let direction = new THREE.Vector3();
+    myModel.getWorldDirection(direction);
+    myModel.position.addScaledVector(direction, moveSpeed);
+  }
+  if (keys.ArrowDown) {
+    let direction = new THREE.Vector3();
+    myModel.getWorldDirection(direction);
+    myModel.position.addScaledVector(direction, -moveSpeed);
+  }
+  if (keys.ArrowLeft && keys.ArrowUp) {
+    myModel.rotation.y += rotateSpeed;
+  }
+  if (keys.ArrowRight && keys.ArrowUp) {
+    myModel.rotation.y -= rotateSpeed;
+  }
+  if (keys.ArrowRight && keys.ArrowDown) {
+    myModel.rotation.y += rotateSpeed;
+  }
+  if (keys.ArrowLeft && keys.ArrowDown) {
+    myModel.rotation.y -= rotateSpeed;
+  }
 }
 
 function animate() {
-    requestAnimationFrame(animate);
-    const worldCoordinates = new THREE.Vector3();
-    camera.getWorldPosition(worldCoordinates);
-    console.log('World Coordinates of Camera:', worldCoordinates);
-    // var modelPosition = new THREE.Vector3();
-    // modelPosition.setFromMatrixPosition(CyberTruck.matrixWorld);
-    // console.log('Model Position:', modelPosition);
-    controls.update();
-    renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+
+  if (myModel) {
+    updatemyModel();
+
+    if (keys.Space) {
+      console.log("SPACE");
+      cameraOffset = new THREE.Vector3(0, 4, -10);
+    }
+
+    const rotatedOffset = cameraOffset.clone();
+    rotatedOffset.applyQuaternion(myModel.quaternion);
+
+    const desiredPosition = myModel.position.clone().add(rotatedOffset);
+
+    camera.position.lerp(desiredPosition, 0.1);
+    camera.lookAt(myModel.position);
+  }
+
+  renderer.render(scene, camera);
 }
 
 animate();
