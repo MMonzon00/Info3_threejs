79cf72f3d6eb81b0fdb7020747c6d1922e2a96d1
MMonzon00 <martinmonzon00@gmail.com>
2024-01-23 03:24:34 -0300

    Last for now

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ directionalLight.position.set(0, 1, 0);
 scene.add(directionalLight);
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
+camera.position.set(10,10,10)
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
